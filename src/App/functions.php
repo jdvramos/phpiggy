6320cad52facf169150aa02f30a042eb51cca954
jdvramos <jonndenzelramos@gmail.com>
2023-10-27 16:42:13 +0800

    10_04_sugar_functions_done

---
+++ src/App/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+// It's named dd as short for dump and die
+function dd(mixed $value) 
+{
+	echo "<pre>";
+	var_dump($value);
+	echo "</pre>";
+	// When we call the dd() we don't want any further code below it to be rendered
+	die();
+}
