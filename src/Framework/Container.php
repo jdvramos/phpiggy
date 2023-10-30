f23eef14e2ad3d512c5379c51f374a44b9cd6852
jdvramos <jonndenzelramos@gmail.com>
2023-10-30 15:01:06 +0800

    12_10_validating_parameters_done

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Framework;
 
-use ReflectionClass;
+use ReflectionClass, ReflectionNamedType;
 use Framework\Exceptions\ContainerException;
 
 class Container
@@ -39,6 +39,32 @@ class Container
 		// Since not all class' constructor function accepts a parameter, add a check
 		if (count($params) === 0) {
 			return new $className;
+		}
+
+		// The dependencies variable is going to store the instances or dependencies required
+		// by our controller. At the moment, all we have is the array of parameters, the goal
+		// is to loop through each parameter, as we do so we'll create an instance for the 
+		// respective parameter
+		$dependencies = [];
+
+		// Since $params returns an array of ReflectionParameter type (ReflectionParameter[])
+		// it's safe to assume that $param is type of ReflectionParameter. The ReflectionParameter
+		// class has methods for viewing information on a specific parameter
+		foreach ($params as $param) {
+			// Get the name of the parameter
+			$name = $param->getName();
+			// Get the data type of the parameter
+			$type = $param->getType();
+			// We are only accepting parameters that are type hinted
+			if (!$type) {
+				throw new ContainerException("Failed to resolve class {$className} because param {$name} is missing a type hint.");
+			}
+			// We are only accepting parameters that have a type of ReflectionNamedType and types
+			// that are not built in to PHP. A parameter has the ReflectionNamedType when there 
+			// is only one type hint
+			if (!$type instanceof ReflectionNamedType || $type->isBuiltin()) {
+				throw new ContainerException("Failed to resolve class {$className} because invalid param name.");
+			}
 		}
 
 		dd($params);
