4b53d1c40b49ea2324ca5ebb51fd80d335845059
jdvramos <jonndenzelramos@gmail.com>
2023-10-30 17:49:51 +0800

    12_20_supporting_dependency_injection_in_middleware_done

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -51,7 +51,7 @@ class Router
 			$action = fn () => $controllerInstance->$function();
 
 			foreach ($this->middlewares as $middleware) {
-				$middlewareInstance = new $middleware;
+				$middlewareInstance = $container ? $container->resolve($middleware) : new $middleware;
 				$action = fn () => $middlewareInstance->process($action);
 			}
 
