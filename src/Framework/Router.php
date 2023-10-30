baa5d7b21580d8c581dec5ccceefab13d35c9dae
jdvramos <jonndenzelramos@gmail.com>
2023-10-30 16:21:29 +0800

    12_14_supporting_router_middleware_done

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -7,6 +7,7 @@ namespace Framework;
 class Router
 {
 	private array $routes = [];
+	private array $middlewares = [];
 
 	public function add(string $method, string $path, array $controller)
 	{
@@ -49,5 +50,10 @@ class Router
 			// If it finds the method, it would allow us to invoke the method like any other method
 			$controllerInstance->$function();
 		}
+	}
+
+	public function addMiddleware(string $middleware)
+	{
+		$this->middlewares[] = $middleware;
 	}
 }
