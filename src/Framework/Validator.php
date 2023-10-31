97dd0a7b98af133b12866b3b89befd9eff3c0f5a
jdvramos <jonndenzelramos@gmail.com>
2023-10-31 17:01:34 +0800

    13_08_applying_rules_to_fields_done

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -9,6 +9,21 @@ use Framework\Contracts\RuleInterface;
 // Not only our validator can use the default rules but users of 
 // our framework can also add their own rules to the validator as long
 // as the rule follows the RuleInterface contract
+
+// Our Validator class is only responsible on how to store the rules and how
+// to perform validations. It is not responsible on which field to validate 
+// and what rule to use to validate that field from, this is the responsibility
+// of the Application to provide for the framework which is in 
+// the ValidatorService class.
+
+// As a rule of thumb, the Framework should be considered tools, they should know
+// the 'How' but not the 'What'. For example, our Router knows how to store routes
+// and dispatch routes. However, it doesn't know what routes should be available in 
+// the application. It's up to our Application to register routes (registerRoutes). 
+// The same idea applies to our Container, our Container knows how to store
+// dependencies and how to instantiate them. On the other hand, it doesn't
+// have an immediate list of dependencies, our Application is responsible for
+// registering the dependencies (container-definitions)
 class Validator
 {
 	private array $rules = [];
@@ -20,8 +35,18 @@ class Validator
 		$this->rules[$alias] = $rule;
 	}
 
-	public function validate(array $formData)
+	public function validate(array $formData, array $fields)
 	{
-		dd($formData);
+		foreach ($fields as $fieldName => $rules) {
+			foreach ($rules as $rule) {
+				$ruleValidator = $this->rules[$rule];
+
+				if ($ruleValidator->validate($formData, $fieldName, [])) {
+					continue;
+				}
+
+				echo "Error";
+			}
+		}
 	}
 }
