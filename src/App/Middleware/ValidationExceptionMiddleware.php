59798ef32a3b5af3caab9b12485431050c82bd57
jdvramos <jonndenzelramos@gmail.com>
2023-10-31 18:47:57 +0800

    13_13_redirection_with_headers_done

--- src/App/Middleware/ValidationExceptionMiddleware.php
+++ src/App/Middleware/ValidationExceptionMiddleware.php
@@ -9,8 +9,22 @@ use Framework\Exceptions\ValidationException;
 
 class ValidationExceptionMiddleware implements MiddlewareInterface
 {
+	// We wrapped the $next() function to a try block since the AuthController, for example,
+	// uses the ValidatorService, and the ValidatorService uses the Validator class. In the
+	// Validator's validate() function there's a possibility that a ValidationException error 
+	// could be thrown when one or more fields failed to pass the validation. In the case that
+	// there's a ValidationException error thrown, the catch block here will catch the error
+	// and perform a redirect to the /register page
+
+	// Keep in mind that this middleware still runs BEFORE and not after the controller.
+	// Adding a try/catch is a trick so that we don't have to create a middleware that 
+	// will only run after the controller
 	public function process(callable $next)
 	{
-		$next();
+		try {
+			$next();
+		} catch (ValidationException $e) {
+			redirectTo("/register");
+		}
 	}
 }
--- src/App/functions.php
+++ src/App/functions.php
@@ -17,3 +17,11 @@ function e(mixed $value): string
 {
 	return htmlspecialchars((string) $value);
 }
+
+function redirectTo(string $path)
+{
+	// The default HTTP method is GET
+	header("Location: {$path}");
+	http_response_code(302);
+	exit;
+}
