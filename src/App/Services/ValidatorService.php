6959a848f3e9a75b576c19087f244d6314a51bef
jdvramos <jonndenzelramos@gmail.com>
2023-10-31 15:49:27 +0800

    13_04_understanding_services_done

---
+++ src/App/Services/ValidatorService.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+// It is a common practice to make Services injectable, add this to container-definitions.php
+class ValidatorService
+{
+}
