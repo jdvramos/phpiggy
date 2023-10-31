111f654ab21d55100831cd8daaff48cba43f6bbe
jdvramos <jonndenzelramos@gmail.com>
2023-10-31 16:32:51 +0800

    13_07_registering_a_rule_done

--- src/App/Services/ValidatorService.php
+++ src/App/Services/ValidatorService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Framework\Validator;
+use Framework\Rules\{RequiredRule};
 
 // It is a common practice to make Services injectable, add this to container-definitions.php
 class ValidatorService
@@ -14,6 +15,10 @@ class ValidatorService
 	public function __construct()
 	{
 		$this->validator = new Validator();
+		// By default, we are not going to register rules with our framework. 
+		// It's possible that developers may prefer to create their own
+		// 'required' rule. This means every rule must be registered by the App
+		$this->validator->add('required', new RequiredRule());
 	}
 
 	public function validateRegister(array $formData)
