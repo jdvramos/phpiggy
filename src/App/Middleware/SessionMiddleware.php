6ed75218759fbdf3fe770ecfb903c3ca8743a964
jdvramos <jonndenzelramos@gmail.com>
2023-11-01 08:24:18 +0800

    13_18_handling_session_exceptions_done

--- src/App/Middleware/SessionMiddleware.php
+++ src/App/Middleware/SessionMiddleware.php
@@ -5,12 +5,25 @@ declare(strict_types=1);
 namespace App\Middleware;
 
 use Framework\Contracts\MiddlewareInterface;
+use App\Exceptions\SessionException;
 
 // By default, PHP disables session. In order to use it we must run the session_start() first
 class SessionMiddleware implements MiddlewareInterface
 {
 	public function process(callable $next)
 	{
+		// Session might be activated by a package you installed by Composer that uses sessions
+		// Multiple sessions are possible and that's not what we want
+		if (session_status() === PHP_SESSION_ACTIVE) {
+			throw new SessionException("Session already active.");
+		}
+
+		// If this returns true, the data has already been sent to the browser. Therefore, we
+		// can't active a session
+		if (headers_sent()) {
+			throw new SessionException("Headers already sent.");
+		}
+
 		session_start();
 		$next();
 	}
