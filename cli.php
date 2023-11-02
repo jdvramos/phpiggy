c8a7b74ce92434ebb81c14c0b377456d337bc43c
jdvramos <jonndenzelramos@gmail.com>
2023-11-02 23:36:03 +0800

    14_12_creating_a_dsn_string_done

--- cli.php
+++ cli.php
@@ -1,3 +1,26 @@
 <?php
 
-echo "Hello world!";
+/*
+	composer run-script phpiggy
+*/
+
+/*
+	The mysql driver is compatible with MariaDB
+*/
+$driver = 'mysql';
+
+/*
+	The http_build_query() was introduced for building URLs. Luckily, it  
+	can be used for building DSN strings. However, the default separator is
+	'&', change this by using named arguments and passing the value ';' as
+	the arg_separator
+*/
+$config = http_build_query(data: [
+	'host' => 'localhost',
+	'port' => 3306,
+	'dbname' => 'phpiggy',
+], arg_separator: ';');
+
+$dsn = "{$driver}:{$config}";
+
+echo $dsn;
